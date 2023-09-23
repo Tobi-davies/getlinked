36a69289fa7c737557b36c2cf38c0e6b679a3908
Tobi-davies <adebayotobiloba9@gmail.com>
2023-09-23 16:12:58 +0100

    footer

--- src/layouts/footer/footer.jsx
+++ src/layouts/footer/footer.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { FaFacebookF } from "react-icons/fa6";
+import { BsInstagram } from "react-icons/bs";
+import { RiTwitterXFill } from "react-icons/ri";
+import { BiLogoLinkedin } from "react-icons/bi";
+import { HiLocationMarker } from "react-icons/hi";
+import { MdPhoneInTalk } from "react-icons/md";
 import "./footer.scss";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer_container">
-        <div className="col-7">
+        <div className="col-12 col-sm-7 first">
           <div>
             <div className="logo">
               <span>get</span>
@@ -17,28 +23,48 @@ const Footer = () => {
               young and talented individuals in the field of technology
             </p>
           </div>
-          <p>Terms of Use | Privacy Policy</p>
+          <p>
+            Terms of Use <span>|</span> Privacy Policy
+          </p>
         </div>
-        <div className="col-3">
-          <ul>
+        <div className="col-12 col-sm-5 col-lg-3">
+          <ul className="footer_links">
             <li>Useful Links</li>
             <li>Overview</li>
             <li>Timeline</li>
             <li>FAQs</li>
             <li>Register</li>
+            <li>
+              Follow us
+              <div>
+                <BsInstagram />
+                <RiTwitterXFill />
+                <FaFacebookF fontSize={15} />
+                <BiLogoLinkedin fontSize={17} />
+              </div>
+            </li>
           </ul>
-          <div>Follow us</div>
+          <div></div>
         </div>
-        <div className="col-2">
-          {" "}
+        <div className="col-12 col-lg-2 footer_contact">
           <ul>
             <li>Contact Us</li>
 
-            <li>+234 679 81819</li>
-            <li>27,Alara Street Yaba 100012 Lagos State</li>
+            <li>
+              {" "}
+              <MdPhoneInTalk /> <span>+234 679 81819</span>
+            </li>
+            <li>
+              <HiLocationMarker />{" "}
+              <span>27,Alara Street Yaba 100012 Lagos State</span>
+            </li>
           </ul>
         </div>
       </div>
+
+      <span className="footer_rights">
+        All rights reserved. © getlinked Ltd.
+      </span>
     </footer>
   );
 };
